4335f4242f8bfbf648ffcd2a89cce9458559699a
chichunchen <chi.chun_chen844@icloud.com>
2015-01-19 00:16:57 +0800

    fix bugs in vim.sh

--- vim.sh
+++ vim.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # backup old vim files
-mv ~/.vim ~/.vim.orig
-mv ~/.vimrc ~/.vimrc.orig
+mv ~/.vim ~/dotfiles/backup/.vim.orig
+mv ~/.vimrc ~/dotfiles/backup/.vimrc.orig
 
 # link vim files to home directory
 mkdir ~/.vim
 cp bundles.vim ~/.vim
-ln -s .vimrc ~/.vim
+ln .vimrc ~/.vimrc
 
 # setup Vundle
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
