c6ff63c8ecac054a4749edb8aaacdd16b77a5b08
chichunchen <chichunchen844@gmail.com>
2015-06-01 10:28:43 +0000

    Fix error in git.sh

--- git-config/git.sh
+++ git-config/git.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [ -f ~/.gitignore ]; then
-    mv ~/.gitignore ~/gitignore.bak
+    mv ~/.gitignore ~/.gitignore.bak
 fi
-cp ~/.dotfile/git-config/.gitignore ~/gitignore
+cp ~/.dotfile/git-config/gitignore ~/.gitignore
 
 if [ -f ~/.gitconfig ]; then
     mv ~/.gitconfig ~/.gitconfig.bak
 fi
-cp ~/.dotfile/git-config/.gitconfig ~/.gitconfig
+cp ~/.dotfile/git-config/gitconfig ~/.gitconfig
 
 echo "Finish git setup work."
