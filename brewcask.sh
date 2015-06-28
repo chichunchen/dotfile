2bd11777ec22bbb476f15ebd0a80d18275312c1c
chichunchen <chichunchen844@gmail.com>
2015-06-29 02:44:39 +0800

    Add brewcask.

---
+++ brewcask.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# homebrew-cask
+brew tap phinze/homebrew-cask
+brew install brew-cask
+
+# browser
+brew install cask google-chrome
+brew install cask firefox-aurora
+brew install cask welly
+brew insatll cask dropbox
+
+# development
+brew cask install sublime-text
+brew cask install java
+brew cask install android-studio
+brew cask install xquartz
+brew cask install filezilla
+brew cask install iterm2
+brew cask install virtualbox
+brew cask install slack
+brew cask install github
+brew cask install macdown
+
+# other
+brew cask install the-unarchiver
+brew cask install  transmission
+brew cask install vlc
+brew cask install flux
+brew cask install spotify
+brew cask install android-file-transfer
