2eab454bb28639e4436a14a0cf26b8263a472887
chichunchen <chichunchen844@gmail.com>
2017-06-19 11:08:45 +0800

    Install new vim.

--- linux-software.sh
+++ linux-software.sh
@@ -1,3 +1,5 @@
 sudo apt-get update
 sudo apt-get dist-upgrade
-sudo apt-get install zsh gdb g++ make vim zsh tmux curl wget valgrind
+sudo apt-get upgrade
+sudo add-apt-repository ppa:jonathonf/vim
+sudo apt-get install build-essential zsh vim gdb g++ make vim zsh tmux curl wget valgrind cmake
