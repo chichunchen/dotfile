54337e930e5c4646b0e7f901b574a8ca61a3c20b
chichunchen <chichunchen844@gmail.com>
2017-07-11 17:13:27 +0800

    Update must-install software.

--- linux-software.sh
+++ linux-software.sh
@@ -2,4 +2,6 @@ sudo apt-get update
 sudo apt-get dist-upgrade
 sudo apt-get upgrade
 sudo add-apt-repository ppa:jonathonf/vim
-sudo apt-get install build-essential zsh vim gdb g++ make vim zsh tmux curl wget valgrind cmake
+sudo apt-get install build-essential zsh vim gdb g++ make vim zsh tmux curl wget valgrind cmake 
+sudo apt-get install linux-tools-common linux-tools-generic
+sudo apt-get install cppcheck astyle colordiff gnuplot
